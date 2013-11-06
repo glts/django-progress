1a9cd7a0c62564d4f4debd06709fd74d1cf3b9c2
glts <676c7473@gmail.com>
2013-11-06 21:51:40 +0100

    Fixed deprecated exclude on ModelForm

--- forms.py
+++ forms.py
@@ -3,6 +3,6 @@ from django.forms.models import modelform_factory
 from .models import Task, Challenge, Routine
 
 
-TaskForm = modelform_factory(Task, exclude=('topic',))
-ChallengeForm = modelform_factory(Challenge, exclude=('topic',))
-RoutineForm = modelform_factory(Routine, exclude=('topic',))
+TaskForm = modelform_factory(Task, fields=('name', 'description', 'tags'))
+ChallengeForm = modelform_factory(Challenge, fields=('name', 'description', 'tags'))
+RoutineForm = modelform_factory(Routine, fields=('name', 'description', 'tags'))
